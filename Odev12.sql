8bff695f44f21a11a0d82c414ebd1569eee7fc2c
mustafa2285 <75934522+mustafa2285@users.noreply.github.com>
2021-07-25 19:32:59 +0300

    Create Odev12.sql

---
+++ Odev12.sql
@@ -0,0 +1,16 @@
+-- 1.film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE length > 
+(SELECT  AVG(length) FROM film);
+
+-- 2.film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE rental_rate = ANY
+(SELECT  MAX(rental_rate) FROM film);
+
+-- 3.film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT rental_rate, replacement_cost FROM film
+WHERE rental_rate = ANY
+(SELECT  MIN(rental_rate)  FROM film)  
+AND replacement_cost = ANY
+(SELECT  MIN(replacement_cost)  FROM film) ;
