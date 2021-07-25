4a5b02646ce370759868e3f07e8742386dd05c3b
mustafa2285 <75934522+mustafa2285@users.noreply.github.com>
2021-07-25 21:03:08 +0300

    Update Odev12.sql

--- Odev12.sql
+++ Odev12.sql
@@ -19,3 +19,4 @@ AND replacement_cost = ANY
 SELECT customer.first_name, customer.last_name, payment.customer_id ,COUNT(*) FROM payment
 JOIN customer ON customer.customer_id = payment.customer_id
 GROUP BY payment.customer_id , customer.first_name, customer.last_name;
+ORDER BY COUNT(*) DESC;
